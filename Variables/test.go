800896494cf3c2072a97e64caed01ed12f8c8610
KvngRagnar <emaikwungbede@gmail.com>
2026-04-09 15:27:43 +0100

    .

---
+++ Variables/test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, d = 6, "Hello", 67788, "ghghj"
+	x := 2
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(x)
+}
