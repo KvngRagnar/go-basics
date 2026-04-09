c6358ea88dadcef683b3d59b95b514578d2145b3
KvngRagnar <emaikwungbede@gmail.com>
2026-04-09 11:52:17 +0100

    .

---
+++ inferred variables.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var good int
+	var bad string
+	var better bool
+	var best float32
+	var gee float64
+	fmt.Println(good)
+	fmt.Println(bad)
+	fmt.Println(better)
+	fmt.Println(best)
+	fmt.Println(gee)
+}
